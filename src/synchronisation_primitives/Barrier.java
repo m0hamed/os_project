d9f7a6a049436668faab0e3ca14d53eb40b368d4
Nourhan Mohamed <nourhan.abdeltawab@gmail.com>
2013-03-12 15:00:57 +0200

    added upper bound var to Barriers to be able to reset count

--- src/synchronisation_primitives/Barrier.java
+++ src/synchronisation_primitives/Barrier.java
@@ -2,15 +2,18 @@ package synchronisation_primitives;
 
 public class Barrier {
 	private int count;
+	private final int upperBound;
 	
 	public Barrier(int initial) {
 		count = initial;
+		upperBound = initial;
 	}
 	
 	public synchronized void wait_on_barrier() {
 		count --;
 		if(count==0){
 			this.notifyAll();
+			count = upperBound;
 		} else {
 			try {
 				this.wait();
